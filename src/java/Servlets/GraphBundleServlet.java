1a9001f4e7c3fb8affd06e1ab76200926dd3467a
Philipp <paschnell@dundee.ac.uk>
2017-09-25 15:09:00 +0100

    added table for unique users (#23)
    * Tweaks to MVC test*.* files. Added graph showing unique users per Outlet.
    
    * added table for unique users

--- src/java/Servlets/GraphBundleServlet.java
+++ src/java/Servlets/GraphBundleServlet.java
@@ -21,33 +21,6 @@ import javax.servlet.http.HttpServletResponse;
 public class GraphBundleServlet extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet GraphBundleServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet GraphBundleServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
      * Handles the HTTP <code>GET</code> method.
      *
      * @param request servlet request
@@ -83,7 +56,10 @@ public class GraphBundleServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+      
+        // Not currently used.
+        RequestDispatcher rd = request.getRequestDispatcher("/graphBundleJSP.jsp");
+        rd.forward(request, response);
     }
 
     /**
